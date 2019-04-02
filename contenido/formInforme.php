54deb71d1e8381d0333be79b68572e702cb4e1c1
Nicolas Muñoz <Nicolas Muoz@Nicolasxb>
2019-04-01 23:02:08 -0300

    algo

--- contenido/formInforme.php
+++ contenido/formInforme.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Ejercicio Tipo Prueba</title>
+        <title></title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <link href="css/estiloprincipal.css" rel="stylesheet" type="text/css"/>
@@ -17,7 +17,9 @@ and open the template in the editor.
         <div id="contenedor">
             <div id="titulo"></div>
             <div id="menu"><?php include('menu.php');?></div>
-            <div id="contenido"></div>
+                <div id="contenido">
+                    
+                </div>
         </div>
         
         <?php
